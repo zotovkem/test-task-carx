833c4aad0b5257d56a09b30700111e4d0384a7ce
ZotovES <zotoves@it2g.ru>
2020-04-08 11:49:43 +0700

    докер

--- src/docker_build_and_run_backend.cmd
+++ src/docker_build_and_run_backend.cmd
@@ -1,11 +1,12 @@
-﻿chcp 65001
+﻿rem cd ..
 
-mvn clean package -DskipTests=true
+rem mvn clean package -DskipTests=true
 
-call docker build --target carxapp -t carx:latest .
-call docker build --target postgres -t carx_postgres:latest .
+call docker build --target carx-app -t carx:latest .
+call docker build --target carx_redis -t carx_redis:latest .
+call docker build --target carx_postgres -t carx_postgres:latest .
 
-cd ..
+rem cd ..
 rem Чистим volume docker образа postgres, иначе не отрабатывает скрипт init.sql.
 call docker volume rm -f backend_pg_data_volume
 rem Запускаем докер контейнеры.
